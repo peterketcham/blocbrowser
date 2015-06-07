b5328c888ed1b0e40a032169948530f1fc8b6c60
Peter Ketcham <peter.ketcham@gmail.com>
2015-06-06 21:44:03 -0500

    Functional web browser

--- BlocBrowser/WebBrowserViewController.h
+++ BlocBrowser/WebBrowserViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  WebBrowserViewController.h
 //  BlocBrowser
 //
 //  Created by Peter Ketcham on 6/5/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface WebBrowserViewController : UIViewController
 
 @end
-
