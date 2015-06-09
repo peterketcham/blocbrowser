3363ee667c4de9716916e93f13e7457a7312e64a
Peter Ketcham <peter.ketcham@gmail.com>
2015-06-08 22:06:58 -0500

    Automatically clear history when the app is closed

--- BlocBrowser/WebBrowserViewController.h
+++ BlocBrowser/WebBrowserViewController.h
@@ -10,4 +10,8 @@
 
 @interface WebBrowserViewController : UIViewController
 
+/* Replaces the web view with a fresh one, erasing all history. Also updates the URL field and toolbar buttons appropriately. */
+
+- (void) resetWebView;
+
 @end
